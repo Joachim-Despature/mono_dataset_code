14dd68dc41125b219ed1ef0020b1893f826e1ea8
Joachim-Despature <joachim.despature@hotmail.fr>
2024-05-25 09:29:37 +0200

    renamed aruco

---
+++ aruco/utils/CMakeFiles/aruco_board_pix2meters.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joachim/VO/mono_dataset_code/aruco-1.3.0/utils/aruco_board_pix2meters.cpp" "utils/CMakeFiles/aruco_board_pix2meters.dir/aruco_board_pix2meters.cpp.o" "gcc" "utils/CMakeFiles/aruco_board_pix2meters.dir/aruco_board_pix2meters.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ aruco/utils_hrm/CMakeFiles/aruco_hrm_create_dictionary.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for aruco_hrm_create_dictionary.
+# This may be replaced when dependencies are built.
