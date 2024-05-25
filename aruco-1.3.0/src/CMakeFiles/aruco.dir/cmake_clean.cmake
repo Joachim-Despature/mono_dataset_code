852f924c794aaf1ad379317be5526cfaaadaf768
Joachim-Despature <joachim.despature@hotmail.fr>
2024-05-25 09:06:52 +0200

    Changed Aruco

---
+++ aruco-1.3.0/src/CMakeFiles/aruco.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  ".1.3"
+  "CMakeFiles/aruco.dir/ar_omp.cpp.o"
+  "CMakeFiles/aruco.dir/ar_omp.cpp.o.d"
+  "CMakeFiles/aruco.dir/arucofidmarkers.cpp.o"
+  "CMakeFiles/aruco.dir/arucofidmarkers.cpp.o.d"
+  "CMakeFiles/aruco.dir/board.cpp.o"
+  "CMakeFiles/aruco.dir/board.cpp.o.d"
+  "CMakeFiles/aruco.dir/boarddetector.cpp.o"
+  "CMakeFiles/aruco.dir/boarddetector.cpp.o.d"
+  "CMakeFiles/aruco.dir/cameraparameters.cpp.o"
+  "CMakeFiles/aruco.dir/cameraparameters.cpp.o.d"
+  "CMakeFiles/aruco.dir/chromaticmask.cpp.o"
+  "CMakeFiles/aruco.dir/chromaticmask.cpp.o.d"
+  "CMakeFiles/aruco.dir/cvdrawingutils.cpp.o"
+  "CMakeFiles/aruco.dir/cvdrawingutils.cpp.o.d"
+  "CMakeFiles/aruco.dir/highlyreliablemarkers.cpp.o"
+  "CMakeFiles/aruco.dir/highlyreliablemarkers.cpp.o.d"
+  "CMakeFiles/aruco.dir/marker.cpp.o"
+  "CMakeFiles/aruco.dir/marker.cpp.o.d"
+  "CMakeFiles/aruco.dir/markerdetector.cpp.o"
+  "CMakeFiles/aruco.dir/markerdetector.cpp.o.d"
+  "CMakeFiles/aruco.dir/subpixelcorner.cpp.o"
+  "CMakeFiles/aruco.dir/subpixelcorner.cpp.o.d"
+  "libaruco.pdb"
+  "libaruco.so"
+  "libaruco.so.1.3"
+  "libaruco.so.1.3.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aruco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
